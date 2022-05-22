3f91ec69cf773d940da50935a961ddfd7b1a87ce
Aravind A <aravindkerala1@gmail.com>
2022-05-22 14:25:29 +0530

    adding nav.js file

---
+++ public/js/nav.js
@@ -0,0 +1,22 @@
+// Part 2 Point 6
+
+const nav= [
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+];
+
+export default nav;
